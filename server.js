2a3eb086ee5d38b4fadc8c928ed2eb08a64c0d6f
AAlkinani1986 <90129610+AAlkinani1986@users.noreply.github.com>
2024-04-02 23:10:41 +1100

    Call Bcrypt and create ASYNC, and SYNC code

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+const bcrypt = require('bcrypt');
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
@@ -11,12 +12,29 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log("Hash:", hash);
+    bcrypt.compare(someOtherPlaintextPassword, hash, (err, result) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log("Passwords match:", result);
+    });
+});
 
 //END_ASYNC
 
 //START_SYNC
-
+const salt = bcrypt.genSaltSync(saltRounds);
+const hashSync = bcrypt.hashSync(myPlaintextPassword, salt);
+console.log("Sync Hash:", hashSync);
+const resultSync = bcrypt.compareSync(someOtherPlaintextPassword, hashSync);
+console.log("Sync Passwords match:", resultSync);
 
 
 //END_SYNC
